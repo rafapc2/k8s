817eee9dc00683a108c707b7615edd3444aff547
Rafael Prudencio <rprudencio@MacBook-Pro-de-equipamiento.local>
2019-06-02 22:36:18 -0400

    actualizacion de comandos

--- lab01-basic-pod/cmd.sh
+++ lab01-basic-pod/cmd.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+
+minikube addons enable metrics-server
+
+kubectl get apiservices
+
+
 echo "creando namespace"
 kubectl create -f namespaces.yml
 
 echo "iniciando creacion de POD"
-kubectl create -f busybox.yml --namespace=dev1-mdw
+kubectl create -f deployment.yml --namespace=dev1-mdw
+
+
+kubectl config set-context --current --namespace=gdev1-mdw
